3dbc46db5a84dd8ad72e8d99b7dc1f4cac8e8df3
Irina1302 <imikhailenko92@gmail.com>
2021-02-09 20:18:39 +0200

    cypress_test2

--- cypress/integration/tests.spec.js
+++ cypress/integration/tests.spec.js
@@ -1,4 +1,11 @@
-const { describe } = require("mocha");
+it ('Signin flow', () =>{
+  cy.visit('https://react-redux.realworld.io/#/login')
 
-describe();
-cy
+  cy.get(':nth-child(1) > .form-control').type('testest1@gmail.com')
+  cy.get(':nth-child(2) > .form-control').type('123456789')
+
+  cy.get('.btn').click()
+
+  cy.get('.navbar').contains('testest11').should('exist')
+
+})
